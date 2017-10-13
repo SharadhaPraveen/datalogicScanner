b8da925d00300f3600fe87c5f629074b9fe66c10
SharadhaPraveen <sharadha.krishnamoorthy@gmail.com>
2017-10-13 12:53:02 +0100

    Add files via upload

--- www/dlscanner.js
+++ www/dlscanner.js
@@ -1,5 +1,29 @@
-window.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "DLScanner", "echo", [str]);
+    var exec = cordova.require("cordova/exec");
+ 
+
+ function DLScanner() {
+
+}
+DLScanner.prototype.scanBarcode = function (successCallback, errorCallback, config) {
+	console.log("Inside dlscanner.js");
+	try{
+      exec(
+	  function(result)
+	  { 
+	   console.log("result "+result);
+	   successCallback(result);
+	   }, 
+	  function(error)
+	  {
+	  console.log(error);
+	  errorCallback(error);
+	  },
+	  'DLScanner', "scanBarcode", null);
+	  }catch(exception){
+		console.log(exception);
+	}
+	console.log("After calling java class");
 };
+
+var DLScanner = new DLScanner();
+module.exports = DLScanner;
